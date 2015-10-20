6b93474b399eef5b1ea29294d2fe081532086095
Egga (Jan Hartung) <github@egga.de>
2015-10-20 23:46:11 +0200

    update aliases

---
+++ config/aliases.sh
@@ -0,0 +1,13 @@
+alias what='git status'
+alias pull='git pull'
+alias push='git push'
+alias fetch='git fetch'
+
+alias really='git'
+alias very='git'
+alias please='git'
+
+alias yolo='git commit -am "DEAL WITH IT" && git push -f origin master'
+
+alias ll='ls -l'
+alias ..='cd ..'

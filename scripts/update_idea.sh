4ac9082a88f88bf5ff8c923c9c1cc1c19c5c6bcd
Egga (Jan Hartung) <github@egga.de>
2015-11-05 15:33:00 +0100

    add script to update idea

---
+++ scripts/update_idea.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+ARCHIVE=ideaIU-15.0.tar.gz
+cd /two/bin/intellij/
+
+cp ~/Downloads/$ARCHIVE /two/bin/intellij/
+tar -xzf $ARCHIVE
+
+
+DIR=`ls -1dt */ | head -n 1`
+echo $DIR
+
+
+links=( idea.png ../idea.png ../idea.sh )
+for LINKED_FILE in "${links[@]}"
+do
+    rm -f $LINKED_FILE
+    ln -s /two/bin/intellij/${DIR}bin/`basename $LINKED_FILE` $LINKED_FILE
+done
+
+
+rm $ARCHIVE
